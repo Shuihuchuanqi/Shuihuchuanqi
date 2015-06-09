c7e408320be3bb37beb08b0461c8f879d8290431
CallMeEric <ericwang_1992@outlook.com>
2015-06-09 17:23:17 +0800

    equipsData
    装备信息

---
+++ EquipsData.lua
@@ -0,0 +1,39 @@
+local EquipsData = { 
+	[1]={ 
+		name = "武器", 
+		level = 1,
+		icon = "iconpath", 
+		star = 1, 
+		property = 10, 
+		level = 1,
+		equipType = "weapon",
+	},
+	[2]={ 
+		name = "铠甲", 
+		level = 1,
+		icon = "iconpath", 
+		star = 1, 
+		property = 10, 
+		level = 1,
+		equipType = "armour",
+	},
+	[3]={ 
+		name = "头盔", 
+		level = 1,
+		icon = "iconpath", 
+		star = 1, 
+		property = 8, 
+		level = 1,
+		equipType = "helmet",
+	},
+	[4]={ 
+		name = "靴子", 
+		level = 1,
+		icon = "iconpath", 
+		star = 1, 
+		property = 0.02, 
+		level = 1,
+		equipType = "shoes",
+	},
+}
+return EquipsData
