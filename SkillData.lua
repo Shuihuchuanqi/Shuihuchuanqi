db170f13668ca8c84efa78a0d09a612818b3c56b
CallMeEric <ericwang_1992@outlook.com>
2015-06-09 11:17:16 +0800

    skillDemo
    需要完善

---
+++ SkillData.lua
@@ -0,0 +1,14 @@
+local Skills = {
+	[301] = {
+		["skill1"] = {
+			icon = "iconpath",
+			level = 1,
+			attack = 30,
+			name = "神龙摆尾",
+			description = "攻击前排所有敌人",
+			kind = "attackFrontForAll",
+			rate = 5,
+			duration = 1,
+		},
+	},
+}
