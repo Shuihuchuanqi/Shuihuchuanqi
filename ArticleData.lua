07dfc49eb75afd01cfceff0ee94054f475ee1f9f
CallMeEric <ericwang_1992@outlook.com>
2015-06-09 19:09:18 +0800

    article
    articleData

---
+++ ArticleData.lua
@@ -0,0 +1,52 @@
+local ArticleData = { 
+	[101]={ 
+		name = "物品1",
+		icon = "iconpath", 
+		price = 100, 
+	},
+	[102]={ 
+		name = "物品2",
+		icon = "iconpath", 
+		price = 200, 
+	},
+	[103]={ 
+		name = "物品3",
+		icon = "iconpath", 
+		price = 300, 
+	},
+	[104]={ 
+		name = "武器碎片",
+		icon = "iconpath", 
+		price = 100, 
+		level = 1,
+		class = "weapon",
+	},
+	[105]={ 
+		name = "铠甲碎片",
+		icon = "iconpath", 
+		price = 100, 
+		level = 1,
+		class = "armous",
+	},
+	[106]={ 
+		name = "头盔碎片",
+		icon = "iconpath", 
+		price = 100, 
+		level = 1,
+		class = "helmet",
+	},
+	[107]={ 
+		name = "靴子碎片",
+		icon = "iconpath", 
+		price = 100, 
+		level = 1,
+		class = "shoes",
+	},
+	[108]={ 
+		name = "能量晶石",
+		icon = "iconpath", 
+		price = 500, 
+		level = 1,
+	},
+}
+return ArticleData
